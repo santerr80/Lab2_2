f1234179297d602c6ad769a5f480d8dca70e3329
santerr80 <santerr80@mail.ru>
2024-11-22 13:26:31 +0700

    Black format

--- myflow.py
+++ myflow.py
@@ -7,9 +7,9 @@ from sklearn.datasets import load_diabetes
 
 # Загрузка данных
 data = load_diabetes()
-X_train, X_test, y_train, y_test = train_test_split(data.data, data.target,
-                                                    test_size=0.2,
-                                                    random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    data.data, data.target, test_size=0.2, random_state=42
+)
 
 mlflow.set_tracking_uri("http://localhost:5000")
 mlflow.set_experiment("diabetes")
@@ -39,7 +39,7 @@ with mlflow.start_run(experiment_id=experiment_id) as run1:
     mlflow.log_param("max_depth", None)
     mlflow.log_metrics(metrics)
     mlflow.sklearn.log_model(rf1, "model")
-    
+
     # Регистрация модели
     model_uri1 = f"runs:/{run1.info.run_id}/model"
     model_details1 = mlflow.register_model(model_uri=model_uri1, name="diabetes_model")
@@ -62,7 +62,7 @@ with mlflow.start_run(experiment_id=experiment_id) as run2:
     mlflow.log_param("max_depth", 10)
     mlflow.log_metrics(metrics)
     mlflow.sklearn.log_model(rf2, "model")
-    
+
     # Регистрация модели
     model_uri2 = f"runs:/{run2.info.run_id}/model"
     model_details2 = mlflow.register_model(model_uri=model_uri2, name="diabetes_model")
