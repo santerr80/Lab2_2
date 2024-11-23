958bbe8741f5206ce967a80b7fb6323da9d22b34
santerr80 <santerr80@mail.ru>
2024-11-23 16:02:42 +0700

    Graph metrics

--- garph_report.py
+++ garph_report.py
@@ -3,28 +3,43 @@ import mlflow.tracking
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Подключитесь к вашему MLflow серверу (если используете удаленный сервер)
+# Setup mlflow
 mlflow.set_tracking_uri("http://localhost:5000")
 
-# Получите данные метрик для всех запусков эксперимента
+# Get the experiment ID
 experiment_name = "diabetes"
 experiment = mlflow.get_experiment_by_name(experiment_name)
 experiment_id = experiment.experiment_id
 
+# Get the runs
 runs = mlflow.search_runs(experiment_ids=[experiment_id])
 
 pd.set_option('display.max_columns', None)
-
 print(runs.head())
 
-# Извлеките метрики из запусков
+# Get the metrics r2
+metrics = runs[["tags.mlflow.runName", "metrics.r2"]]
+
+# Plot the metrics
+plt.figure(figsize=(10, 6))
+plt.plot(metrics["tags.mlflow.runName"], metrics["metrics.r2"], marker='o')
+plt.xlabel('Run ID')
+plt.ylabel('Metric Value')
+plt.title(f'Metric R2 Overview for {experiment_name}')
+plt.grid(True)
+plt.savefig("reports/metrics_r2.png")
+plt.close()
+
+
+# Get the metrics mse
 metrics = runs[["tags.mlflow.runName", "metrics.mse"]]
 
-# Постройте график
+# Plot the metrics
 plt.figure(figsize=(10, 6))
 plt.plot(metrics["tags.mlflow.runName"], metrics["metrics.mse"], marker='o')
 plt.xlabel('Run ID')
 plt.ylabel('Metric Value')
-plt.title(f'Metric Overview for {experiment_name}')
+plt.title(f'Metric MSE Overview for {experiment_name}')
 plt.grid(True)
-plt.show()
+plt.savefig("reports/metrics_mse.png")
+plt.close()
